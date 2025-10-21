bdb269cdefe51b8bd402206f0a5123479ab69f0a
MaxV16 <maks.karakulinn@gmail.com>
2025-10-21 11:49:59 +0100

    Formatted release dates

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -30,6 +30,7 @@ export default function MovieCard({ movie, action }) {
     e.preventDefault();
     addToFavorites(movie);
   };
+  
 
 
   return (
@@ -62,7 +63,11 @@ export default function MovieCard({ movie, action }) {
           <Grid size={{ xs: 6 }}>
             <Typography variant="h6" component="p">
               <CalendarIcon fontSize="small" />
-              {movie.release_date}
+              {new Date(movie.release_date).toLocaleDateString('en-GB', {
+                  day: '2-digit',
+                  month: 'short',
+                  year: '2-digit',
+                })}
             </Typography>
           </Grid>
           <Grid size={{ xs: 6 }}>
